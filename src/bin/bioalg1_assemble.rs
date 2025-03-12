00b20b18eafca29adac969f4d4153b29b5eddfaf
Allan Rasmussen <allanbrondum@gmail.com>
2025-03-12 15:20:12 +0100

    fix

--- src/bin/bioalg1_assemble.rs
+++ src/bin/bioalg1_assemble.rs
@@ -3,6 +3,7 @@ use rosalind::string::sc_supstr;
 use rosalind::util::fasta_polymers;
 use std::fs::File;
 use std::io::Write;
+use std::time::Instant;
 
 fn main() {
     let min_olap = 20;
@@ -10,7 +11,10 @@ fn main() {
     let output_path = "src/bin/bioalg1_assemble_out.txt";
 
     let polymers = fasta_polymers(input_path).collect_vec();
+
+    let start = Instant::now();
     let dnas = sc_supstr(polymers.into_iter().map(|pol| pol.polymer), min_olap);
+    println!("assemble elapsed: {:?}", start.elapsed());
 
     let mut file = File::create(output_path).unwrap();
     for (idx, dna) in dnas.iter().enumerate() {
