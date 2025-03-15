9169ba80097d27d41b0262e2d60cd99b4033b7bd
Allan Rasmussen <allanbrondum@gmail.com>
2025-03-15 10:00:21 +0100

    trie

--- src/alphabet_model.rs
+++ src/alphabet_model.rs
@@ -1,41 +1,38 @@
+use std::fmt::Debug;
 use generic_array::ArrayLength;
-use generic_array::typenum::{Add1, B1, Unsigned};
-use std::fmt::Display;
-use std::marker::PhantomData;
-use std::ops::Add;
 
 pub trait AlphabetT {
-    type N: ArrayLength;
-    type Char: CharT;
+    type N: ArrayLength+ Debug;
+    type Char: CharT + Debug;
 }
 
-pub trait CharT {
+pub trait CharT: Copy {
     fn index(self) -> usize;
 }
 
-pub struct WithTerminator<A> {
-    alphabet: PhantomData<A>,
-}
-
-impl<A: AlphabetT> AlphabetT for WithTerminator<A>
-where
-    A::N: Add<B1>,
-    Add1<A::N>: ArrayLength,
-{
-    type N = Add1<A::N>;
-    type Char = CharOrTerminator<A>;
-}
-
-pub enum CharOrTerminator<A: AlphabetT> {
-    Char(A::Char),
-    Terminator,
-}
-
-impl<A: AlphabetT> CharT for CharOrTerminator<A> {
-    fn index(self) -> usize {
-        match self {
-            CharOrTerminator::Char(ch) => ch.index(),
-            CharOrTerminator::Terminator => A::N::to_usize(),
-        }
-    }
-}
+// pub struct WithTerminator<A> {
+//     alphabet: PhantomData<A>,
+// }
+//
+// impl<A: AlphabetT> AlphabetT for WithTerminator<A>
+// where
+//     A::N: Add<B1>,
+//     Add1<A::N>: ArrayLength,
+// {
+//     type N = Add1<A::N>;
+//     type Char = CharOrTerminator<A>;
+// }
+//
+// pub enum CharOrTerminator<A: AlphabetT> {
+//     Char(A::Char),
+//     Terminator,
+// }
+//
+// impl<A: AlphabetT> CharT for CharOrTerminator<A> {
+//     fn index(self) -> usize {
+//         match self {
+//             CharOrTerminator::Char(ch) => ch.index(),
+//             CharOrTerminator::Terminator => A::N::to_usize(),
+//         }
+//     }
+// }
--- src/string/suffix_trie.rs
+++ src/string/suffix_trie.rs
@@ -1,38 +1,92 @@
-use crate::alphabet_model::{ AlphabetT};
-use generic_array::typenum::{Add1, B1};
-use generic_array::{ArrayLength, GenericArray};
-use std::ops::Add;
+use crate::alphabet_model::{AlphabetT, CharT};
+use generic_array::GenericArray;
 
-// struct Node<A: AlphabetExtendableWithTerminatortT>
-// where
-//     A::N: Add<B1>,
-//     Add1<A::N>: ArrayLength,
-// {
-//     char: <WithTerminator<A> as AlphabetT>::Char,
-//     children: GenericArray<Option<Box<Node<A>>>, <WithTerminator<A> as AlphabetT>::N>,
-// }
+#[derive(Debug)]
+pub struct SuffixTrie<A: AlphabetT> {
+    root: Node<A>,
+}
 
-// pub fn build<A: AlphabetT>(&[Char]) where
-//     A::N: Add<B1>,
-//     Add1<A::N>: ArrayLength,{
-//
-// }
+#[derive(Debug)]
+struct Node<A: AlphabetT> {
+    children: GenericArray<Option<Box<Edge<A>>>, A::N>,
+    terminal: Option<Terminal>,
+}
+
+impl<A: AlphabetT> Node<A> {
+    fn new() -> Self {
+        Self {
+            children: Default::default(),
+            terminal: None,
+        }
+    }
+}
+
+#[derive(Debug)]
+struct Terminal {
+    suffix_index: usize,
+}
+
+#[derive(Debug)]
+struct Edge<A: AlphabetT> {
+    char: A::Char,
+    target: Node<A>,
+}
+
+pub fn build<A: AlphabetT>(s: &[A::Char]) -> SuffixTrie<A> {
+    let mut trie = SuffixTrie {
+        root: Node::new(),
+    };
+
+    for i in 0..s.len() - 1 {
+        insert_rec(i, &s[i..], &mut trie.root);
+    }
+
+    trie
+}
+
+fn insert_rec<A: AlphabetT>(suffix: usize, s: &[A::Char], node: &mut Node<A>) {
+    match s.split_first() {
+        None => {
+            assert!(node.terminal.is_none());
+            node.terminal = Some(Terminal {
+                suffix_index: suffix,
+            });
+        }
+        Some((ch, s_rest)) => {
+            let mut edge = Edge {
+                char: *ch,
+                target: Node::new(),
+            };
+            insert_rec(suffix, s_rest, &mut edge.target);
+            node.children[ch.index()] = Some(Box::new(edge));
+        }
+    }
+}
 
 #[cfg(test)]
 mod test {
-    use crate::alphabet_model::AlphabetT;
-    use generic_array::typenum::{U2, UInt};
+    use crate::alphabet_model::{AlphabetT, CharT};
+    use generic_array::typenum::U2;
 
+    #[derive(Debug, Copy, Clone, Eq, PartialEq, Hash)]
     enum Char {
         A,
         B,
     }
 
+    impl CharT for Char {
+        fn index(self) -> usize {
+            match self {
+                Char::A => 0,
+                Char::B => 1,
+            }
+        }
+    }
+
     struct Alphabet;
 
-    // impl AlphabetT for Alphabet {
-    //     type N = U2;
-    //     type IdxType = u8;
-    //     type Char = Char;
-    // }
+    impl AlphabetT for Alphabet {
+        type N = U2;
+        type Char = Char;
+    }
 }
